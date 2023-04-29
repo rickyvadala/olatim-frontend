c472203f61972c7a6ea1274af3a5637c5fea03f4
rickyvadala <rickyvadala@gmail.com>
2023-04-29 16:19:51 -0300

    first commit

---
+++ src/__mock__/footer.ts
@@ -0,0 +1,69 @@
+import {OlaFooterProps} from "@/components/organisms/OlaFooter/OlaFooter";
+
+export const footer: OlaFooterProps = {
+  "data": [
+    {
+      "title": "About",
+      "links": [
+        {
+          "label": "Features",
+          "link": "#"
+        },
+        {
+          "label": "Pricing",
+          "link": "#"
+        },
+        {
+          "label": "Support",
+          "link": "#"
+        },
+        {
+          "label": "Forums",
+          "link": "#"
+        }
+      ]
+    },
+    {
+      "title": "Project",
+      "links": [
+        {
+          "label": "Contribute",
+          "link": "#"
+        },
+        {
+          "label": "Media assets",
+          "link": "#"
+        },
+        {
+          "label": "Changelog",
+          "link": "#"
+        },
+        {
+          "label": "Releases",
+          "link": "#"
+        }
+      ]
+    },
+    {
+      "title": "Community",
+      "links": [
+        {
+          "label": "Join Discord",
+          "link": "#"
+        },
+        {
+          "label": "Follow on Twitter",
+          "link": "#"
+        },
+        {
+          "label": "Email newsletter",
+          "link": "#"
+        },
+        {
+          "label": "GitHub discussions",
+          "link": "#"
+        }
+      ]
+    }
+  ]
+}
